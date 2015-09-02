58a4f3c8ea05f9905e5cf96176da3240a7d1823c
derekolsson <derekolsson@gmail.com>
2015-09-02 13:19:05 -0600

    Q1 done
    Iterators created
    takes inputs
    showing outputs started

--- Hoja6.java
+++ Hoja6.java
@@ -11,7 +11,7 @@ public class Hoja6{
 		System.out.println("Please choose an implementation.\n1. HashSet \n2. TreeSet \n3. LinkedHashSet.");
 		System.out.println("\nPlease choose an option: 1, 2, or 3\n");
 		
-		//loop while input is valid
+		//loop while input is invalid
 		boolean loop = true;
 		choice = "";
 		while (loop){
@@ -27,19 +27,87 @@ public class Hoja6{
 			}
 		}
 		
+		//create the three sets using the choice made
 		Set javaSet = setFact.getSet(choice);
 		Set webSet = setFact.getSet(choice);
 		Set cellSet =  setFact.getSet(choice);
 		
+		//choice cleared and ready for new use
 		choice = "";
+		
+		//variables for "answer" and "name"
+		//name:		name of new user
+		//answer:	yes or no to developer types
+		String answer;
+		String name;
+		
+		//loop until final choice is "q"
 		while (!choice.equalsIgnoreCase("q")){
-			String name = "";
+			//get name....clear it just to avoid issues
+			name = "";
 			System.out.println("Please enter new user name: ");
 			name = in.nextLine();
 			
-			String answer = "";
-			System.out.println("Are the a java developer? y/n : ");
+			//get what they develop in
+			//only take y or n for answer
+			//clear answer each time just to avoid problems
+			
+			//java
+			answer = "";	
+			while (!answer.equalsIgnoreCase("y")&&!answer.equalsIgnoreCase("n")){
+				System.out.println("Are they a java developer?\nPlease enter either y or n : ");
+				answer = in.nextLine();
+			}
+			if (answer.equalsIgnoreCase("y")){
+				javaSet.add(name);
+			}
+			
+			//web
+			answer = "";
+			while (!answer.equalsIgnoreCase("y")&&!answer.equalsIgnoreCase("n")){
+				System.out.println("Are they a web developer?\nPlease enter either y or n : ");
+				answer = in.nextLine();
+			}
+			if (answer.equalsIgnoreCase("y")){
+				webSet.add(name);
+			}
+			
+			//cellular
+			answer = "";
+			while (!answer.equalsIgnoreCase("y")&&!answer.equalsIgnoreCase("n")){
+				System.out.println("Are they a cellular developer?\nPlease enter either y or n : ");
+				answer = in.nextLine();
+			}
+			if (answer.equalsIgnoreCase("y")){
+				cellSet.add(name);
+			}
+			
+			//anything to continue
+			//q or Q to quit
+			System.out.println("\nHit enter to continue.\nTo quit, enter q");
+			choice = in.nextLine();
 		}
+		
+		//Print out all those desired results
+		//Gonna need iterators
+		Iterator javaIt = javaSet.iterator();
+		Iterator webIt = webSet.iterator();
+		Iterator cellIt = cellSet.iterator();
+		
+		//1. Union de los tres
+		// iterate javaSet, then look for contains() in the other sets
+		System.out.println("Users with experience in Java, Web and Cell: ");
+		while (javaIt.hasNext()){
+			Object a = javaIt.next();
+			if (webSet.contains(a) && cellSet.contains(a)){
+				System.out.println(a);
+			}
+		}
+		
+		
+		
+		
+		
 	}
 }	
 	
